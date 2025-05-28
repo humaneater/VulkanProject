79bf4f2d88ef951405c1d75cf2be09e0174aad23
liuqingyu <liuqingyu126441@pwrd.com>
2025-05-28 09:50:24 +0800

    验证层添加

--- VulkanProject/VulkanProject/FirstTriangle.h
+++ VulkanProject/VulkanProject/FirstTriangle.h
@@ -1,16 +1,40 @@
 #pragma once
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <cstring>
 class FirstTriangle
 {
 public:
 	void Run();
+	const uint32_t width = 800;
+	const uint32_t height = 600;
+	const std::vector<const char*> validationLayers = {
+		"VK_LAYER_KHRONOS_validation",
+	};
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
 
 private:
+	void InitWindow();
 	void InitVulkan();
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+	void SetupDebugMessenger();
 	void MainLoop();
 	void CleanUp();
+	void CreateInstance();
+	bool CheckValidationLayerSupport();
+	std::vector<const char*> getRequiredExtensions();
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+	GLFWwindow* window;
+	VkInstance instance;
+	VkDebugUtilsMessengerEXT debugMessenger;
 };
 
